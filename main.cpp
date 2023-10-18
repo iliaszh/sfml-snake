c7d66d6d0a5954a746e8bcde05dc73382f86f5ec
ilias <sabazov14@gmail.com>
2023-10-18 21:37:18 +0600

    it works

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+auto main() -> int {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+    window.setVerticalSyncEnabled(true);
+    window.setFramerateLimit(60);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color::Black);
+        // draw everything here...
+        // window.draw(...);
+        window.display();
+    }
+
+    return 0;
+}
